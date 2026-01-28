a02f412938ece9eeb721665a1e237b8462c591ce
c444jmtz <jmartin13245@gmail.com>
2026-01-28 15:18:28 -0500

    Testing

--- discussion_2_git/src/main.rs
+++ discussion_2_git/src/main.rs
@@ -3,11 +3,11 @@ fn parity(x: i64) -> i64 {
 }
 
 fn add(a: i64, b: i64) -> i64{
-    return 0; // Change this line
+    return a + b; // Change this line
 }
 
 fn main() {
     let a = 5;
     let b = 6;
     println!("Parity of {} + {} is {}", a, b, parity(add(a, b)));
-}
+} 
